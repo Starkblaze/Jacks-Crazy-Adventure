53ca98740b9f9aa352cfcdd22ffb39a84d623eaa
Starkblaze <starkblaze@hotmail.com>
2018-12-15 21:47:52 -0800

    Added Woombat Behaviour

---
+++ Assets/Scripts/WoombatBehaviour.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WoombatBehaviour : MonoBehaviour {
+	
+	// Variables for the speed movement of the Woombat
+	public float speed = 5f;
+	bool facingRight = true;
+	
+	// Initialize Animator and Rigidbody 
+	Animator anim;
+	Rigidbody2D rigid2D;
+
+	// Use this for initialization
+	void Start () {
+		
+		// Initialize Animator and Rigidbody
+		anim = GetComponent<Animator>();
+		rigid2D = GetComponent<Rigidbody2D>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	void FixedUpdate () {
+		
+		// Moving left or right
+		rigid2D.velocity = new Vector2(speed, rigid2D.velocity.y);
+		anim.SetFloat("Speed", Mathf.Abs(speed));
+		
+	}
+}
